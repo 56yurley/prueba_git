bc7cecbd20648dc5720d495f65ac71c5a7293855
Yurley Gallego <yurley.cetad@epfac.edu.co>
2021-03-02 20:43:10 -0500

    Se agregan campos

--- src/main/java/FormCalculator.java
+++ src/main/java/FormCalculator.java
@@ -1,11 +1,25 @@
 import javax.swing.*;
 
 public class FormCalculator extends JFrame{
-    private JPanel panel1;
-    private JTextField textField1;
+    private JPanel panel1 = new JPanel();
+
+    private JLabel lblNum1 = new JLabel("Number One");
+    private JLabel lblNum2 = new JLabel("Number Two");
+    private JTextField textField1 = new JTextField(3);
+    private JTextField textField2 = new JTextField(3);
+    private JButton btnResult = new JButton("Calculator");
+
+    private JLabel result = new JLabel();
 
     public FormCalculator(){
+        panel1.add(lblNum1);
+        panel1.add(textField1);
+        panel1.add(lblNum2);
+        panel1.add(textField2);
+        panel1.add(btnResult);
+        panel1.add(result);
 
+        this.add(panel1);
         this.setSize(500, 200);
         this.setTitle("Basic Calculator");
         //this.add(panel1);
